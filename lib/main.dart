2027bf0cfe37974061c908c3159c604c65235485
Raihan <17radf@gmail.com>
2022-09-17 09:12:07 +0700

    add: interactivity and some boxes

--- lib/main.dart
+++ lib/main.dart
@@ -9,93 +9,170 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Widget titleSection = Container(
+    Widget upperSection = Container(
       padding: const EdgeInsets.all(32),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Expanded(
-              child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                padding: const EdgeInsets.only(bottom: 8),
-                child: const Text(
-                  'Oeschinen Lake Campground',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-              ),
-              Text(
-                'Kandersteg, Switzerland',
-                style: TextStyle(
-                  color: Colors.grey[500],
-                ),
-              ),
-            ],
-          )),
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
+          const Expanded(
+              child: Text(
+                  "ketik 1 jika anda capek dengan hidup, yahahahah jamsut fristel hehe ku kira takkan ada kendala ku kira lorem ipsum dolor sit jamett",
+                  softWrap: true)),
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              children: const [Icon(Icons.place), Text("ligma")],
+            ),
           ),
-          const Text('41')
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              children: const [Icon(Icons.access_alarm), Text("sayang")],
+            ),
+          ),
         ],
       ),
-    );
-
-    Color color = Theme.of(context).primaryColor;
-
-    Column _buildButtonColumn(Color color, IconData icon, String label) {
-      return Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(icon, color: color),
-          Container(
-            margin: const EdgeInsets.only(top: 8),
-            child: Text(
-              label,
-              style: TextStyle(
-                  fontSize: 12, fontWeight: FontWeight.w400, color: color),
-            ),
-          )
-        ],
-      );
-    }
-
-    Widget buttonSection = Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        _buildButtonColumn(color, Icons.call, 'CALL'),
-        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
-        _buildButtonColumn(color, Icons.share, 'SHARE'),
-      ],
-    );
-
-    Widget textSection = const Padding(
-      padding: EdgeInsets.all(32),
-      child: Text(
-        "Khianati sebisa dirimu mengkhianati, karena ku pastikan andai dulu kau tak belum tentu kisah kita berakhir bahagia kisah yang aowkaokwoakwoakwok lorem ipsum dolor sit jamet",
-        softWrap: true,
-        ),
     );
 
     return MaterialApp(
-      title: 'Test lmaoo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Test lmaoo'),
-        ),
-        body: ListView(children: [
-          Image.asset(
-            'images/lake.jpg',
-            width: 600,
-            height: 240,
-            fit: BoxFit.cover,
+        debugShowCheckedModeBanner: false,
+        title: 'Test lmaoo',
+        home: Scaffold(
+          appBar: AppBar(
+            leading: Icon(Icons.dashboard),
+            title: const Text("Belajar melepaskan dirinya"),
+            actions: [
+              Row(
+                children: const [
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Icon(Icons.search),
+                  )
+                ],
+              )
+            ],
+            backgroundColor: Colors.blue[500],
           ),
-          titleSection,
-          buttonSection,
-          textSection
-        ]),
-      ),
+          body: Column(
+            children: [
+              Image.network(
+                "https://docs.flutter.dev/assets/images/dash/dash-fainting.gif",
+                width: 600,
+                fit: BoxFit.cover,
+              ),
+              upperSection,
+              const FavoriteWidget(),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                              color: Colors.blue, shape: BoxShape.circle),
+                        ),
+                        Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                              color: Colors.blue, shape: BoxShape.circle),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(color: Colors.redAccent),
+                    ),
+                    Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(color: Colors.redAccent),
+                    ),
+                    Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(color: Colors.redAccent),
+                    ),
+                    Column(
+                      children: [
+                        Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                              color: Colors.blue, shape: BoxShape.circle),
+                        ),
+                        Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                              color: Colors.blue, shape: BoxShape.circle),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () => print("hehe"),
+            backgroundColor: Colors.pink[500],
+            child: const Icon(Icons.add),
+          ),
+        ));
+  }
+}
+
+class FavoriteWidget extends StatefulWidget {
+  const FavoriteWidget({Key? key}) : super(key: key);
+
+  @override
+  State<FavoriteWidget> createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 69;
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(0),
+          child: IconButton(
+            padding: const EdgeInsets.all(0),
+            alignment: Alignment.centerRight,
+            icon: (_isFavorited
+                ? const Icon(Icons.star)
+                : const Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: SizedBox(child: Text('$_favoriteCount')),
+        )
+      ],
     );
   }
 }
