1009ccbd5b50021aad815ad7c296b660260584c1
Raihan <17radf@gmail.com>
2022-09-22 21:23:13 +0700

    add: named routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Named Routes Demo',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const MyApp(),
+        '/second': (context) => const SecondApp(),
+      },
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -11,120 +21,130 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     Widget upperSection = Container(
       padding: const EdgeInsets.all(32),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      child: Column(
         children: [
-          const Expanded(
-              child: Text(
-                  "ketik 1 jika anda capek dengan hidup, yahahahah jamsut fristel hehe ku kira takkan ada kendala ku kira lorem ipsum dolor sit jamett",
-                  softWrap: true)),
-          Padding(
-            padding: EdgeInsets.all(10),
-            child: Column(
-              children: const [Icon(Icons.place), Text("ligma")],
-            ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              const Expanded(
+                  child: Text(
+                      "ketik 1 jika anda capek, yahahahah jamsut fristel hehe ku kira takkan ada kendala ku kira lorem ipsum dolor sit jamett",
+                      softWrap: true)),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  children: const [Icon(Icons.place), Text("ligma")],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  children: const [Icon(Icons.access_alarm), Text("sayang")],
+                ),
+              ),
+            ],
           ),
-          Padding(
-            padding: EdgeInsets.all(10),
-            child: Column(
-              children: const [Icon(Icons.access_alarm), Text("sayang")],
-            ),
-          ),
+          Row(
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, "/second");
+                  },
+                  child: Icon(Icons.abc))
+            ],
+          )
         ],
       ),
     );
 
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Test lmaoo',
-        home: Scaffold(
-          appBar: AppBar(
-            leading: Icon(Icons.dashboard),
-            title: const Text("Belajar melepaskan dirinya"),
-            actions: [
-              Row(
-                children: const [
-                  Padding(
-                    padding: EdgeInsets.all(10),
-                    child: Icon(Icons.search),
-                  )
-                ],
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.dashboard),
+        title: const Text("Belajar melepaskan dirinya"),
+        actions: [
+          Row(
+            children: const [
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Icon(Icons.search),
               )
             ],
-            backgroundColor: Colors.blue[500],
+          )
+        ],
+        backgroundColor: Colors.blue[500],
+      ),
+      body: Column(
+        children: [
+          Image.network(
+            "https://docs.flutter.dev/assets/images/dash/dash-fainting.gif",
+            width: 600,
+            fit: BoxFit.cover,
           ),
-          body: Column(
-            children: [
-              Image.network(
-                "https://docs.flutter.dev/assets/images/dash/dash-fainting.gif",
-                width: 600,
-                fit: BoxFit.cover,
-              ),
-              upperSection,
-              const FavoriteWidget(),
-              Padding(
-                padding: const EdgeInsets.all(10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          const FavoriteWidget(),
+          upperSection,
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
                   children: [
-                    Column(
-                      children: [
-                        Container(
-                          width: 50,
-                          height: 50,
-                          decoration: BoxDecoration(
-                              color: Colors.blue, shape: BoxShape.circle),
-                        ),
-                        Container(
-                          width: 50,
-                          height: 50,
-                          decoration: BoxDecoration(
-                              color: Colors.blue, shape: BoxShape.circle),
-                        ),
-                      ],
+                    Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                          color: Colors.blue, shape: BoxShape.circle),
                     ),
                     Container(
                       width: 50,
                       height: 50,
-                      decoration: BoxDecoration(color: Colors.redAccent),
-                    ),
-                    Container(
-                      width: 50,
-                      height: 50,
-                      decoration: BoxDecoration(color: Colors.redAccent),
-                    ),
-                    Container(
-                      width: 50,
-                      height: 50,
-                      decoration: BoxDecoration(color: Colors.redAccent),
-                    ),
-                    Column(
-                      children: [
-                        Container(
-                          width: 50,
-                          height: 50,
-                          decoration: BoxDecoration(
-                              color: Colors.blue, shape: BoxShape.circle),
-                        ),
-                        Container(
-                          width: 50,
-                          height: 50,
-                          decoration: BoxDecoration(
-                              color: Colors.blue, shape: BoxShape.circle),
-                        ),
-                      ],
+                      decoration: BoxDecoration(
+                          color: Colors.blue, shape: BoxShape.circle),
                     ),
                   ],
                 ),
-              )
-            ],
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () => print("hehe"),
-            backgroundColor: Colors.pink[500],
-            child: const Icon(Icons.add),
-          ),
-        ));
+                Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(color: Colors.redAccent),
+                ),
+                Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(color: Colors.redAccent),
+                ),
+                Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(color: Colors.redAccent),
+                ),
+                Column(
+                  children: [
+                    Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                          color: Colors.blue, shape: BoxShape.circle),
+                    ),
+                    Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                          color: Colors.blue, shape: BoxShape.circle),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => print("hehe"),
+        backgroundColor: Colors.pink[500],
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
 
@@ -173,6 +193,37 @@ class _FavoriteWidgetState extends State<FavoriteWidget> {
           child: SizedBox(child: Text('$_favoriteCount')),
         )
       ],
+    );
+  }
+}
+
+class SecondApp extends StatelessWidget {
+  const SecondApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          child: ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("lol kids"))),
+      appBar: AppBar(
+        leading: Icon(Icons.dashboard),
+        title: const Text("Belajar melepaskan dirinya"),
+        actions: [
+          Row(
+            children: const [
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Icon(Icons.search),
+              )
+            ],
+          )
+        ],
+        backgroundColor: Colors.blue[500],
+      ),
     );
   }
 }
